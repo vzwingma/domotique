3450bad390f1fb8f83d6e544ca278f3c2465d9ee
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2015-05-14 17:02:34 +0200

    Ajout de radionReception

--- commandes-radio/radioEmission/radioEmission.cpp
+++ commandes-radio/radioEmission/radioEmission.cpp
@@ -10,7 +10,7 @@
 /*
 Par Idleman (idleman@idleman.fr - http://blog.idleman.fr)
 Licence : CC by sa
-Toutes question sur le blog ou par mail, possibilité de m'envoyer des bières via le blog
+Edite par vzwingma
 */
 
 using namespace std;
@@ -23,31 +23,32 @@ int interruptor;
 int sender;
 string onoff;
 
+// Log des actions
 void log(string a){
 	//Décommenter pour avoir les logs
-
-	//cout << a << endl;
+	cout << a << endl;
 }
 
 
 
-
+// Scheduler en real time pour utiliser les microsecondes
 void scheduler_realtime() {
 
-struct sched_param p;
-p.__sched_priority = sched_get_priority_max(SCHED_RR);
-if( sched_setscheduler( 0, SCHED_RR, &p ) == -1 ) {
-perror("Failed to switch to realtime scheduler.");
-}
+	struct sched_param p;
+	p.__sched_priority = sched_get_priority_max(SCHED_RR);
+	if( sched_setscheduler( 0, SCHED_RR, &p ) == -1 ) {
+		perror("Failed to switch to realtime scheduler.");
+	}
 }
 
+// Retour au mode standard
 void scheduler_standard() {
 
-struct sched_param p;
-p.__sched_priority = 0;
-if( sched_setscheduler( 0, SCHED_OTHER, &p ) == -1 ) {
-perror("Failed to switch to normal scheduler.");
-}
+	struct sched_param p;
+	p.__sched_priority = 0;
+	if( sched_setscheduler( 0, SCHED_OTHER, &p ) == -1 ) {
+		perror("Failed to switch to normal scheduler.");
+	}
 }
 
 
@@ -58,9 +59,9 @@ perror("Failed to switch to normal scheduler.");
 void sendBit(bool b) {
  if (b) {
    digitalWrite(pin, HIGH);
-   delayMicroseconds(310);   //275 orinally, but tweaked.
+   delayMicroseconds(310);   //275 originally, but tweaked.
    digitalWrite(pin, LOW);
-   delayMicroseconds(1340);  //1225 orinally, but tweaked.
+   delayMicroseconds(1340);  //1225 originally, but tweaked.
  }
  else {
    digitalWrite(pin, HIGH);
@@ -72,11 +73,11 @@ void sendBit(bool b) {
 
 //Calcul le nombre 2^chiffre indiqué, fonction utilisé par itob pour la conversion decimal/binaire
 unsigned long power2(int power){
-unsigned long integer=1;
-for (int i=0; i<power; i++){
-  integer*=2;
-}
-return integer;
+	unsigned long integer=1;
+	for (int i=0; i<power; i++){
+		integer*=2;
+	}
+	return integer;
 } 
 
 //Convertis un nombre en binaire, nécessite le nombre, et le nombre de bits souhaité en sortie (ici 26)
@@ -109,16 +110,16 @@ void itobInterruptor(unsigned long integer, int length)
 //Envoie d'une paire de pulsation radio qui definissent 1 bit réel : 0 =01 et 1 =10
 //c'est le codage de manchester qui necessite ce petit bouzin, ceci permet entre autres de dissocier les données des parasites
 void sendPair(bool b) {
- if(b)
- {
-   sendBit(true);
-   sendBit(false);
- }
- else
- {
-   sendBit(false);
-   sendBit(true);
- }
+	if(b)
+	{
+		sendBit(true);
+		sendBit(false);
+	}
+	else
+	{
+		sendBit(false);
+		sendBit(true);
+	}
 }
 
 
@@ -154,76 +155,74 @@ void transmit(int blnOn)
  // Envoie des 4 derniers bits, qui représentent le code interrupteur, ici 0 (encode sur 4 bit donc 0000)
  // nb: sur  les télécommandes officielle chacon, les interrupteurs sont logiquement nommés de 0 à x
  // interrupteur 1 = 0 (donc 0000) , interrupteur 2 = 1 (1000) , interrupteur 3 = 2 (0100) etc...
-  for(i=0; i<4;i++)
+ for(i=0; i<4;i++)
  {
- if(bit2Interruptor[i]==0){
-	sendPair(false);
- }else{
-	sendPair(true);
- }
+	if(bit2Interruptor[i]==0){
+		sendPair(false);
+	}else{
+		sendPair(true);
+	}
 }
  
  digitalWrite(pin, HIGH);   // coupure données, verrou
  delayMicroseconds(275);      // attendre 275µs
  digitalWrite(pin, LOW);    // verrou 2 de 2675µs pour signaler la fermeture du signal
-
 }
 
 
 
-
+// Démarrage du programme
 int main (int argc, char** argv)
 {
-
+	// Vérification des permissions
 	if (setuid(0))
 	{
-
 		perror("setuid");
 		return 1;
-
 	}
 
 
 	scheduler_realtime();
 
-	log("Demarrage du programme");
+	log(">>>*<<<");
 	pin = atoi(argv[1]);
 	sender = atoi(argv[2]);
 	interruptor = atoi(argv[3]);
 	onoff = argv[4];
+	
+	string logcmd = "   Commande " + onoff + " sur telecommande " + argv[2] + " / bouton : " + argv[3] + " (pin #" + argv[1] + ")";
+	log(logcmd);
+
 	//Si on ne trouve pas la librairie wiringPI, on arrête l'execution
     if(wiringPiSetup() == -1)
     {
-        log("Librairie Wiring PI introuvable, veuillez lier cette librairie...");
+        log("ERREUR : Librairie Wiring PI introuvable, veuillez lier cette librairie...");
         return -1;
 
     }
     pinMode(pin, OUTPUT);
-	log("Pin GPIO configure en sortie");
+	log("   Pin GPIO correctement configure en sortie");
 
-	itob(sender,26);            // convertion du code de l'emetteur (ici 8217034) en code binaire
+	itob(sender,26);            // convertion du code de l'emetteur en code binaire
 	itobInterruptor(interruptor,4);
 	
 	
 	if(onoff=="on"){
-	 system("/etc/lcd/screen -p \"Radio signal ON...\"");
-	 log("envois du signal ON");
+	 log("> Envoi du signal ON");
 	 for(int i=0;i<5;i++){
 		 transmit(true);            // envoyer ON
 		 delay(10);                 // attendre 10 ms (sinon le socket nous ignore)
 	 }
 
 	}else{
-	 system("/etc/lcd/screen -p \"Radio signal OFF...\"");
-	 log("envois du signal OFF");
+	 log("< Envoi du signal OFF");
 	 for(int i=0;i<5;i++){
 		 transmit(false);           // envoyer OFF
 		 delay(10);                // attendre 10 ms (sinon le socket nous ignore)
 	 }
 	}
 
-	 log("fin du programme");    // execution terminée.
-
+	 log("<<<*>>>");    // execution terminée.
 
 	scheduler_standard();
 }
