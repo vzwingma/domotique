5d05d90d15bfb5d2a9985e334f84578f1470f412
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2015-05-15 14:12:03 +0200

    Tests de ping sur les 2 smartphones

--- commandes-radio/domoticz/scripts/lua/script_time_ping_smartphone.lua
+++ commandes-radio/domoticz/scripts/lua/script_time_ping_smartphone.lua
@@ -1,18 +1,31 @@
 commandArray = {}
-print(" Debut ping Smartphone V (" .. uservariables["IP_Smartphone_V"] .. ")")
+print("Tests de présence des smartphones (" .. uservariables["IP_Smartphone_V"] .. ") & (" .. uservariables["IP_Smartphone_S"] .. ")")
 ping_success=os.execute('ping -c1 ' .. uservariables["IP_Smartphone_V"])
 if ping_success then
-	print("ping Smartphone V success")
-     if ( otherdevices['Smartphone V'] == 'Off') then
-                  print("Smartphone V up")
-                 commandArray['Smartphone V']='On'
+	print("  ping Smartphone V OK")
+	if ( otherdevices['Smartphone V'] == 'Off') then
+		print("	Le smartphone V est présent")
+		commandArray['Smartphone V']='On'
     end
 else
-      if (otherdevices['Smartphone V'] == 'On') then
-                print("ping Smartphone V fail")
-               commandArray['Smartphone V']='Off'
-      end
+	if (otherdevices['Smartphone V'] == 'On') then
+		print("	Le smartphone V est absent")
+		commandArray['Smartphone V']='Off'
+	end
 end
-print(" fin ping Smartphone V ")
+
+ping_success=os.execute('ping -c1 ' .. uservariables["IP_Smartphone_S"])
+if ping_success then
+	print("  ping Smartphone S OK")
+	if ( otherdevices['Smartphone S'] == 'Off') then
+		print("	Le smartphone S est présent")
+		commandArray['Smartphone V']='On'
+	end
+else
+	if (otherdevices['Smartphone S'] == 'On') then
+		print("	Le smartphone S est absent")
+		commandArray['Smartphone S']='Off'
+	end
+end
 return commandArray
 
