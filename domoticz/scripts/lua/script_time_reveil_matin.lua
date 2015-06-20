720d15ed8721c533e94c366545a95dc4eec34b70
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2015-06-20 15:45:36 +0200

    Ajout d'une fonction réveil

--- domoticz/scripts/lua/script_time_reveil_matin.lua
+++ domoticz/scripts/lua/script_time_reveil_matin.lua
@@ -1,12 +1,9 @@
 commandArray = {}
 -- print("[REVEIL] Réveil matin")
 heure_reveil = uservariables["reveil_heure"]
--- Vérification des paramètres du script
-if( heure_reveil == nil) then
-	print("[REVEIL][ERREUR] Les variables {reveil_heure} n'est pas définie dans Domoticz")
-	return 512
-else
--- Lancement des actions seulement la semaine et seulement si quelqu'un est là :
+
+-- # Lancement du réveil #
+function lancementReveil()
 	jour_courant=os.date("%a")
 	heure_courante=os.date("%H:%M")
 	if(heure_courante == heure_reveil) then
@@ -22,5 +19,15 @@ else
 			commandArray['Scene:Bon matin'] = 'On'
 		end
 	end
+end
+
+
+-- Vérification des paramètres du script
+if( heure_reveil == nil) then
+	print("[REVEIL][ERREUR] Les variables {reveil_heure} n'est pas définie dans Domoticz")
+	return 512
+else
+	-- Lancement des actions seulement la semaine et seulement si quelqu'un est là :
+	lancementReveil()
 end
 return commandArray
