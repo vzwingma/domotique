336cf6fa089aee00de00c8b0c804ea2a92073b2e
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2015-06-20 15:45:14 +0200

    Script télécommande : variabilisation du log

--- domoticz/scripts/bash/script_telecommande_chacon.sh
+++ domoticz/scripts/bash/script_telecommande_chacon.sh
@@ -21,12 +21,14 @@ daystamp() {
   date +"%a"
 }
 
-echo  "">> $DOMOTICZ_DIR/logs/script_telecommande_chacon.log
-echo  "$(timestamp) [DI/O] Ecriture sur le pin [$pin] " >> $DOMOTICZ_DIR/logs/script_telecommande_chacon_$(daystamp).log
-echo  "$(timestamp) [DI/O]  Commande $onoff" >> $DOMOTICZ_DIR/logs/script_telecommande_chacon_$(daystamp).log 
+SCRIPT_LOG_DIR="$DOMOTICZ_DIR/logs/script_telecommande_chacon_$(daystamp)log"
+
+echo  "">> $SCRIPT_LOG_DIR
+echo  "$(timestamp) [DI/O] Ecriture sur le pin [$pin] " >> $SCRIPT_LOG_DIR
+echo  "$(timestamp) [DI/O]  Commande $onoff" >> $SCRIPT_LOG_DIR
 
 for b in 1 2 3 4 5
 do
-   sudo $RADIO_DIR/radioEmission $pin $telecommande $bouton $onoff >> $DOMOTICZ_DIR/logs/script_telecommande_chacon_$(daystamp).log
+   sudo $RADIO_DIR/radioEmission $pin $telecommande $bouton $onoff >> $SCRIPT_LOG_DIR
 done
-echo  "$(timestamp) [DI/O] Fin de la commande" >> $DOMOTICZ_DIR/logs/script_telecommande_chacon_$(daystamp).log
+echo  "$(timestamp) [DI/O] Fin de la commande" >> $SCRIPT_LOG_DIR
