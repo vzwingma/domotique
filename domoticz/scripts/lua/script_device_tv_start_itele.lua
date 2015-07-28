c960f1c395ad2a21a5a9a553914a6b5175b1457f
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2015-07-28 18:36:21 +0200

    Gestion de la nouvelle version de la FreeboxTV et l'apparition du guide dans le menu de la télévision

--- domoticz/scripts/lua/script_device_tv_start_itele.lua
+++ domoticz/scripts/lua/script_device_tv_start_itele.lua
@@ -21,6 +21,7 @@ function commandeTeleCanalSat(freeboxCode, freeboxChannel)
 	callUrlFreebox(freeboxCode, "down", false, 1)
 	callUrlFreebox(freeboxCode, "down", false, 1)
 	callUrlFreebox(freeboxCode, "down", false, 1)
+	callUrlFreebox(freeboxCode, "down", false, 1)
 	callUrlFreebox(freeboxCode, "ok", false, 10)
 	-- Découpage de la chaine en chiffre : appui long
 	size=string.len(freeboxChannel)
