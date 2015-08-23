6cac2b58a76179d1fa69c4b44fec1f24d72fafd5
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2015-08-23 12:43:33 +0200

    lecture du fichier DHT 11 mise à jour dans Domoticz

--- domoticz/scripts/lua/script_time_dht11.lua
+++ domoticz/scripts/lua/script_time_dht11.lua
@@ -2,9 +2,8 @@
 commandArray = {}
 
 -- Variables
-id_dht11=uservariables["interrupteur_id_dht11"]
-APPLI_DIR="/home/pi/appli/receptionDHT11/receptionDHT11"
-
+local id_dht11=uservariables["interrupteur_id_dht11"]
+local APPLI_DIR="/home/pi/appli/receptionDHT11/receptionDHT11"
 
 -- LOG
 function log(message)
@@ -19,10 +18,27 @@ function readLine(file)
 	if(f == nil) then
 		return ""
 	else
-		local content = f:read("*line")
+		local content = f:read("*all")
+		log(content)
 		f:close()
 		return content
 	end
+end
+
+-- Fonction de split 
+-- @param inputstr : chaine à splitter
+-- @param sep : séparateur
+function splitString(inputstr, sep)
+        if sep == nil then
+                sep = "%s"
+        end
+        local t={} ; i=1
+        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+                t[i] = str
+				-- log(" Split [" .. i .. "] " .. str)
+                i = i + 1
+        end
+        return t
 end
 
 
@@ -31,9 +47,7 @@ function readFromDHT11()
 	local TMPDIR_DHT11 = "/tmp/dht11.tmp"
 	log("Récupération des valeurs du DHT11")
 	os.execute("sudo " .. APPLI_DIR .. " > " .. TMPDIR_DHT11)
-	local readDHT11Values = readLine(TMPDIR_DHT11)
-	log(">" .. readDHT11Values)
-	
+	return splitString(readLine(TMPDIR_DHT11), '|')
 end
 
 
@@ -43,10 +57,9 @@ if( id_dht11 == nil ) then
 	error("[DHT11] La variable {interrupteur_id_dht11} n'est pas définie dans Domoticz")
 	return 512
 else
-	readFromDHT11()
-	
-	dht11temp=12
-	dht11hydro=123
+	local dht11Values = readFromDHT11()
+	dht11temp=dht11Values[2] / 10
+	dht11hydro=dht11Values[3] / 10
 	-- commande de mise à jour vers Domoticz
 	commandeTH=id_dht11 .. "|0|" .. dht11temp .. ";" .. dht11hydro .. ";1"
 	commandArray['UpdateDevice']=commandeTH
