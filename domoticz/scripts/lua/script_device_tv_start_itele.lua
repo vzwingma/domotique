545a06fa963b02424d55fd697b9dcef72db85d4f
Vincent ZWINGMANN <vincent.zwingmann@gmail.com>
2016-10-23 16:03:03 +0200

    Mise à jour du script de commande Orange Livebox

--- domoticz/scripts/lua/script_device_tv_start_itele.lua
+++ domoticz/scripts/lua/script_device_tv_start_itele.lua
@@ -8,78 +8,86 @@ end
 --
 -- Fonction Commande de la télévision 
 --   pour afficher une chaine sur CanalSat
--- @param freeboxCode : code de la télécommande
--- @param freeboxChannel : Chaine à lancer
-function commandeTeleCanalSat(freeboxCode, freeboxChannel)
+-- @param ip_livebox_tv : code de la télécommande
+-- @param channel_tv : Chaine à lancer
+function commandeTeleCanalSat(ip_livebox_tv, channel_tv)
 	datelog=os.date("%a")
-	log("Démarrage de la FreeboxTV [" .. freeboxCode .. "] sur la chaine " .. freeboxChannel)
-	-- Démarrage
-	callUrlFreebox(freeboxCode, "power", false, 30)
+	log("Démarrage de la Livebox TV [" .. ip_livebox_tv .. "] sur la chaine " .. channel_tv)
+	-- Démarrage / ON
+	callUrlLivebox(ip_livebox_tv, "116", false)
+	pause(10)
 	-- Son à 0
-	callUrlFreebox(freeboxCode, "vol_dec", true, 0.5)
-	callUrlFreebox(freeboxCode, "vol_dec", true, 0.5)
-	callUrlFreebox(freeboxCode, "vol_dec", true, 0.5)
-	callUrlFreebox(freeboxCode, "vol_dec", true, 0.5)
-	callUrlFreebox(freeboxCode, "vol_dec", true, 0.5)
-	-- CanalSat	
-	callUrlFreebox(freeboxCode, "home", false, 0.5)	
-	callUrlFreebox(freeboxCode, "down", false, 0.5)
-	callUrlFreebox(freeboxCode, "down", false, 0.5)
-	callUrlFreebox(freeboxCode, "down", false, 0.5)
-	callUrlFreebox(freeboxCode, "ok", false, 10)
-	-- Découpage de la chaine en chiffre : appui long
-	size=string.len(freeboxChannel)
+	callUrlLivebox(ip_livebox_tv, "114", true)
+	callUrlLivebox(ip_livebox_tv, "114", true)
+	callUrlLivebox(ip_livebox_tv, "114", true)
+	callUrlLivebox(ip_livebox_tv, "114", true)
+	callUrlLivebox(ip_livebox_tv, "114", true)
+	-- CanalSat	normalement déjà configurée
+
+	-- Découpage de la chaine en chiffre
+	size=string.len(channel_tv)
 	for i=1,size,1 
 	do
-		touche=string.sub(freeboxChannel, i, i)
-		callUrlFreebox(freeboxCode, touche, true, 0.5)
+		touche=string.sub(channel_tv, i, i)
+	 	callUrlLivebox(ip_livebox_tv, 512 + touche, false)
 	end
-	callUrlFreebox(freeboxCode, "vol_inc", true, 1)
-	callUrlFreebox(freeboxCode, "vol_inc", false, 1)
-	callUrlFreebox(freeboxCode, "vol_inc", false, 1)
-	callUrlFreebox(freeboxCode, "vol_inc", false, 1)
+	-- Son à +3
+	callUrlLivebox(ip_livebox_tv, "115", true)
+	callUrlLivebox(ip_livebox_tv, "115", false)
+	callUrlLivebox(ip_livebox_tv, "115", false)
 end
 
 
 
--- Fonction d'appel à l'API de la freebox TV
+-- Fonction d'appel à l'API du Orange Livebox Player
 -- Pour lancer une commande : Curl
--- @param freeboxCode : code de la télécommande
+-- @param ip_livebox_tv : IP de la livebox
 -- @param key : touche 
 -- @param appuilong : appui long ? 
--- @param tempo : tempo d'après la commande en seconde
-function callUrlFreebox(freeboxCode, key, appuilong, tempo)
-	url="http://hd1.freebox.fr/pub/remote_control?code=" .. freeboxCode .. "&key=" .. key
+
+function callUrlLivebox(ip_livebox_tv, key, appuilong)
+	baseurl="http://" .. ip_livebox_tv .. ":8080/remoteControl/cmd?operation=01&key=" .. key .. "&mode="
 	if( appuilong == true ) then
-		url=url .. "&long=true"
+		url=baseurl .. "1"
+	else
+		url=baseurl .. "0"
 	end
 	
 	log("Appel de l'URL : [" .. url .. "]")
 	os.execute("curl '".. url .. "'")
 	
+	if( appuilong == true ) then
+		os.execute("sleep 1")
+		url=baseurl .. "2"
+		log("Appel de l'URL : [" .. url .. "]")
+		os.execute("curl '".. url .. "'")
+	end
+end
+
+-- Pause
+-- @param tempo : tempo d'après la commande en seconde
+function pause(tempo)
 	if( tempo > 0 ) then
 		log("  Tempo de " .. tempo .. " secondes")
 		os.execute("sleep ".. tempo)
 	end
 end
 
-
-
 -- Fonction principale
---   Action ssi l'état de Freebox Player est à On
+--   Action ssi l'état de Livebox Player est à On
 --
-if ( devicechanged['Freebox Player'] == 'On' ) then
+if ( devicechanged['Livebox Player'] == 'On' ) then
 	log("Démarrage de la télévision")
 	-- Test de la variable
-	freeboxCode = uservariables["freebox_code"]
-	freeboxChannel = uservariables["freebox_channel"]
-	if( freeboxCode == nil or freeboxChannel == nil ) then
-		error("[ERREUR] Les variables {freebox_code} et {freebox_channel} ne sont pas définies dans Domoticz")
+	ip_livebox_tv = uservariables["ip_livebox_tv"]
+	channel_tv = uservariables["channel_tv"]
+	if( ip_livebox_tv == nil or channel_tv == nil ) then
+		error("[ERREUR] Les variables {ip_livebox_tv} et {channel_tv} ne sont pas définies dans Domoticz")
 		return 512
 	else
 		-- Lancement de la commande télé dans une coroutine
 		co = coroutine.create(function ()
-			commandeTeleCanalSat(freeboxCode, freeboxChannel)
+			commandeTeleCanalSat(ip_livebox_tv, channel_tv)
 			end
 		)
 		coroutine.resume(co)
