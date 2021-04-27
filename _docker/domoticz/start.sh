542d0efe874d81d14232875a9fc0f9a046e3baee
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2021-04-27 20:19:28 +0200

    déplacement de domoticz sur le repo de l'image

--- _docker/domoticz/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [ -z ${TZ+x} ]; then export TZ=Europe/Paris; fi
-rm /etc/localtime
-cd /etc; ln -s /usr/share/zoneinfo/$TZ localtime
-/src/domoticz/domoticz -dbase /config/domoticz.db -log /config/domoticz.log -www 8080 -sslwww 8443 -sslcert /src/domoticz/server_cert.pem
