8a3929066a3021b6472d6ac78410c608e54d2e22
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2021-05-17 21:57:52 +0200

    gestion du mode de domicile

--- domoticz/scripts/dzVents/Scene_3_Soiree.lua
+++ domoticz/scripts/dzVents/Scene_3_Soiree.lua
@@ -11,6 +11,10 @@ return
     execute = function(domoticz, scene)
         -- Fermeture du groupe de volets (quel que soit le mode Domicile)
         domoticz.groups(domoticz.helpers.GROUPE_TOUS_VOLETS).switchOff()
-  
+        -- Activation de la lampe seulement si mode par défaut ou Eté
+        local modeDomicile = domoticz.helpers.getModeDomicile(domoticz)
+        if(modeDomicile == '' or modeDomicile == '_ete') then
+            domoticz.devices(domoticz.helpers.DEVICE_LAMPE_SALON).setLevel(50)
+        end
     end       
 }
--- domoticz/scripts/dzVents/Scene_4_Nuit.lua
+++ domoticz/scripts/dzVents/Scene_4_Nuit.lua
@@ -12,7 +12,8 @@ return
 
         -- Fermeture du groupe de volets  (quel que soit le mode Domicile)
         domoticz.groups(domoticz.helpers.GROUPE_TOUS_VOLETS).switchOff()
-        
+        -- Extinction de la lampe
+        domoticz.devices(domoticz.helpers.DEVICE_LAMPE_SALON).switchOff()
         -- Thermostat pour la nuit, suivant le mode Domicile
         local modeDomicile = domoticz.helpers.getModeDomicile(domoticz)
         local tempNuit = domoticz.variables(domoticz.helpers.VAR_TEMPERATURE_SOIR .. modeDomicile).value
