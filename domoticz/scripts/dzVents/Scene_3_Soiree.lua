d7466061961f45dd846a68223d512413ed2d21b1
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2021-06-10 23:05:38 +0200

    correction du mode été avec la clim

--- domoticz/scripts/dzVents/Scene_3_Soiree.lua
+++ domoticz/scripts/dzVents/Scene_3_Soiree.lua
@@ -14,18 +14,19 @@ return
         local modeDomicile = domoticz.helpers.getModeDomicile(domoticz)
         if(modeDomicile == '' or modeDomicile == '_ete') then
             domoticz.devices(domoticz.helpers.DEVICE_LAMPE_SALON).setLevel(50)
-            
-            -- Maj Level Salons pour relever suivant le paramétrage 
+        end
+        
+        -- Maj Level Salons pour relever suivant le paramétrage 
+        if( modeDomicile == '_ete') then
             local levelVoletsSalon = domoticz.variables(domoticz.helpers.VAR_PRCENT_VOLET_SOIR .. modeDomicile).value
             domoticz.devices(domoticz.helpers.DEVICE_VOLET_SALON_G).setLevel(levelVoletsSalon)
             domoticz.devices(domoticz.helpers.DEVICE_VOLET_SALON_D).setLevel(levelVoletsSalon)
+            domoticz.devices(domoticz.helpers.DEVICE_VOLET_NOUS).setLevel(levelVoletsSalon)
         else 
-            -- Fermeture du groupe de volets Salon 
-            domoticz.groups(domoticz.helpers.GROUPE_VOLETS_SALON).switchOff()
+            -- Fermeture du groupe de volets Salon &  Chambres
+            domoticz.groups(domoticz.helpers.GROUPE_TOUS_VOLETS).switchOff()
         end
 
-        -- Fermeture du groupe de volets Chambres quelque soit le mode Domicile
-        domoticz.groups(domoticz.helpers.GROUPE_VOLETS_CHAMBRES).switchOff()
 
     end       
 }
