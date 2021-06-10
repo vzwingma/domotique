7e2205243de43b74e02ec54a30d6125b518a69c8
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2021-06-10 12:47:05 +0200

    Mutualisation des appels vers la Livebox

--- domoticz/scripts/dzVents/global_data.lua
+++ domoticz/scripts/dzVents/global_data.lua
@@ -119,7 +119,31 @@ return {
         end,
         
         -- ### FONCTIONS HTTP VERS LA LIVEBOX
-        callLiveboxPOST = function (contextId, putData, callbackName, domoticz)
+
+        -- Authentification
+        authenticateToLivebox = function(callbackName, domoticz)
+    
+            domoticz.log("Authentification à la Livebox ORANGE", domoticz.LOG_DEBUG) 
+    
+            local host_livebox = domoticz.variables(domoticz.helpers.VAR_LIVEBOX_HOST).value
+            local login_livebox = domoticz.variables(domoticz.helpers.VAR_LIVEBOX_LOGIN).value
+            local pwd_livebox = domoticz.variables(domoticz.helpers.VAR_LIVEBOX_PWD).value
+            local authData = { ['service'] = 'sah.Device.Information', 
+                               ['method'] = 'createContext',
+                               ['parameters'] = { ['applicationName'] = 'so_sdkut', 
+                                                  ['username'] = login_livebox, 
+                                                  ['password'] = pwd_livebox }}
+            -- Appel de l'authentification
+            local fullcmd = "curl -s -H \"Content-Type: application/x-sah-ws-4-call+json\" -H \"Authorization:X-Sah-Login\" -d '" 
+                            .. domoticz.utils.toJSON(authData) .. "' -c /opt/domoticz/userdata/scripts/dzVents/data/liveboxCookieAuth.cookie -X POST " ..
+                            "'http://" .. host_livebox .. "/ws'"
+
+        	domoticz.executeShellCommand({ 
+        	            command = fullcmd, 
+        	            callback = callbackName })
+        end,  
+        
+        callLiveboxPOST = function (contextId, postData, callbackName, domoticz)
             
             local host_livebox = domoticz.variables(domoticz.helpers.VAR_LIVEBOX_HOST).value
             
@@ -127,15 +151,12 @@ return {
                callbackName = 'global_HTTP_response' 
             end
             domoticz.log("contextId=["..contextId.."]")
-            
-            domoticz.openURL({
-                url = 'http://'..host_livebox..'/ws',
-                method = 'POST',
-                headers = { ['Content-type'] = 'application/x-sah-ws-4-call+json', ['X-Context'] = contextId },
-                postData = putData,
-                callback = callbackName
-            })
-            return
+            local fullcmd = "SESSID=`cat /opt/domoticz/userdata/scripts/dzVents/data/liveboxCookieAuth.cookie  | awk 'END{print}' | awk '{new_var=$(NF-1)\"=\"$(NF); print new_var}'` ; " ..
+            "curl -s -H \"Content-Type: application/x-sah-ws-4-call+json\" -H \"X-Context: " .. contextId .. "\" -d '" .. domoticz.utils.toJSON(postData) .. "' -b \"$SESSID\" -X POST 'http://" .. host_livebox .. "/ws'"
+
+        	domoticz.executeShellCommand({ 
+        	        command = fullcmd, 
+        	        callback = callbackName })            
         end
     }
 }
