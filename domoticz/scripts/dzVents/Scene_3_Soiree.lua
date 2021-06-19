1f10210f01886701b211e1a38e4a8164479b3e2e
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2021-06-19 22:29:54 +0200

    remplacement des groupes par des devices de groupes

--- domoticz/scripts/dzVents/Scene_3_Soiree.lua
+++ domoticz/scripts/dzVents/Scene_3_Soiree.lua
@@ -12,34 +12,40 @@ return
         
         -- Met à jour le niveau du volet, ssi il est plus petit que la valeur existante
         function setVoletsLevelToMinValue(deviceName, levelVoletsSoir)
-            local existingLevel = domoticz.devices(domoticz.helpers.deviceName).level
-            local levelToSet = 0 
-            if(levelVoletsSoir < existingLevel) then
-                levelToSet = levelVoletsSoir
-            else
-                levelToSet = existingLevel
-            end
-            domoticz.devices(deviceName).setLevel(levelToSet)
+            domoticz.devices(deviceName).setLevel(math.min(domoticz.devices(domoticz.helpers.deviceName).level, levelVoletsSoir))
         end
-        
+
         -- Activation de la lampe seulement si mode par défaut ou Eté
-        local modeDomicile = domoticz.helpers.getModeDomicile(domoticz)
-        if(modeDomicile == '' or modeDomicile == '_ete') then
-            domoticz.devices(domoticz.helpers.DEVICE_LAMPE_SALON).setLevel(50)
+        function activationLampeSalon()
+            local modeDomicile = domoticz.helpers.getModeDomicile(domoticz)
+            if(modeDomicile == '' or modeDomicile == '_ete') then
+                domoticz.log("Allumage de la lampe du salon")
+                domoticz.devices(domoticz.helpers.DEVICE_LAMPE_SALON).setLevel(50)
+            else
+                domoticz.log('Personne à la maison, pas d\'allumage des lampes')
+            end
+        end
+    
+        -- Maj Level Salon & Chambre suivant le paramétrage         
+        function activationVolets()
+
+            local levelVoletsSoir = nil
+            if( modeDomicile == '_ete') then
+                levelVoletsSoir = domoticz.variables(domoticz.helpers.VAR_PRCENT_VOLET_SOIR .. modeDomicile).value
+            else 
+                levelVoletsSoir = 0
+            end
+    
+            -- Fermeture du groupe de volets Salon &  Chambres
+            domoticz.log("Fermeture des volets Salon et Chambre : " .. levelVoletsSoir .. "%")
+            setVoletsLevelToMinValue(domoticz.helpers.GROUPE_VOLETS_SALON, levelVoletsSoir)
+            setVoletsLevelToMinValue(domoticz.helpers.DEVICE_VOLET_NOUS, levelVoletsSoir)
+            
+            domoticz.log("Fermeture du volet "..domoticz.helpers.DEVICE_VOLET_BEBE .. " : 0 %")
+            setVoletsLevelToMinValue(domoticz.helpers.DEVICE_VOLET_BEBE, 0)
         end
         
-        -- Maj Level Salons pour relever suivant le paramétrage 
-        if( modeDomicile == '_ete') then
-            local levelVoletsSoir = domoticz.variables(domoticz.helpers.VAR_PRCENT_VOLET_SOIR .. modeDomicile).value
-            setVoletsLevelToMinValue(domoticz.helpers.DEVICE_VOLET_SALON_G, levelVoletsSoir)
-            setVoletsLevelToMinValue(domoticz.helpers.DEVICE_VOLET_SALON_D, levelVoletsSoir)
-            setVoletsLevelToMinValue(domoticz.helpers.DEVICE_VOLET_NOUS, levelVoletsSoir)
-            setVoletsLevelToMinValue(domoticz.helpers.DEVICE_VOLET_BEBE, 0)
-        else 
-            -- Fermeture du groupe de volets Salon &  Chambres
-            domoticz.groups(domoticz.helpers.GROUPE_TOUS_VOLETS).switchOff()
-        end
-
-
+        activationLampeSalon()
+        activationVolets()
     end       
 }
