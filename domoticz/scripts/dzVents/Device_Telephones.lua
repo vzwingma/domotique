3e8e3a092f98862411b2ce3be5635fd2bda55d48
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2021-10-05 19:04:54 +0200

    suivi de la présence par nb de téléphones

--- domoticz/scripts/dzVents/Device_Telephones.lua
+++ domoticz/scripts/dzVents/Device_Telephones.lua
@@ -2,6 +2,9 @@ return {
     on = {
         devices = { 'Equipements Personnels' }
     },
+    data = {
+        previousPresenceTels = { initial = true }
+    },
     logging = {
         level = domoticz.LOG_DEBUG,
         marker = "[Equipements Personnels] "
@@ -9,6 +12,11 @@ return {
     execute = function(domoticz, item)
         -- Notification par SMS lors du changement de nombre de connexions
         local nbTels = domoticz.devices(domoticz.helpers.DEVICE_STATUT_PERSONNAL_DEVICES).sensorValue
-        domoticz.helpers.notify('Nb de téléphones connectés : ' .. nbTels, domoticz)
+        domoticz.log("Nombre de téléphones connectés : " .. nbTels, domoticz.LOG_DEBUG)
+        local presenceTels = nbTels > 0
+        if(presenceTels ~= domoticz.data.previousPresenceTels) then
+            domoticz.helpers.notify('Présence : ' .. presenceTels, domoticz)
+        end
+        domoticz.data.previousPresenceTels = presenceTels
     end
 }
