3211e9d12af3036f20120cfbe1b610182f9f80d3
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2021-10-17 18:25:40 +0200

    Ajout de morgan et morgan body

--- tydom-bridge/app.js
+++ tydom-bridge/app.js
@@ -6,7 +6,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 const {createClient} = require('tydom-client');
 const express = require('express');
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const morganBody = require('morgan-body');
 
 // Port exposé
 const port = process.env.PORT || 9001;
@@ -25,131 +25,92 @@ let webServer;
     const socket = await client.connect();
 
     const app = express();
+    // must parse body before morganBody as body will be logged
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
-    app.use(morgan('combined'))
-
+    // hook morganBody to express app
+    morganBody(app);
+    app.use(morgan(':date[clf] >>> Request  - :method :url'))
+    app.use(morgan(':date[clf] <<< Response - :status :res[content-length] - :response-time ms'))
+    
     // Info
 	app.get('/_info', function (req, res) {
-        console.log("------ Start request : GET /_info");
-
 	    res.send('Le bridge Tydom [ ' + username + ' ] est opérationnel');
-
-        console.log("------ Finished : GET /_info");
 	})
     // INFO Tydom
     app.get('/info', async function(req, res) {
-        console.log("------ Start request : GET /info");
-
         const info = await client.get('/info');
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(info));
-        
-        console.log(JSON.stringify(info));
-        console.log("------ Finished : GET /info");
     })
     // Liste des devices
     .get('/devices/data', async function(req, res) {
-        console.log("------ Start request : GET /devices/data");
-
         const devices = await client.get('/devices/data');
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(devices));
-        console.log(JSON.stringify(devices));
-        console.log("------ Finished : GET /devices/data");
     })
+    // Liste Meta des devices
     .get('/devices/meta', async function(req, res) {
-        console.log("------ Start request : GET /devices/meta");
-
         const devices = await client.get('/devices/meta');
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(devices));
-        console.log(JSON.stringify(devices));
-        console.log("------ Finished : GET /devices/meta");
     })
+    // Liste CMeta des devices
     .get('/devices/cmeta', async function(req, res) {
-        console.log("------ Start request : GET /devices/cmeta");
-
         const devices = await client.get('/devices/cmeta');
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(devices));
-        console.log(JSON.stringify(devices));
-        console.log("------ Finished : GET /devices/cmeta");
     })
+    // Configuration
     .get('/configs/file', async function(req, res) {
-        console.log("------ Start request : GET /configs/file");
-
         const configs = await client.get('/configs/file');
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(configs));
-        console.log(JSON.stringify(configs));
-        console.log("------ Finished : GET /configs/file");
     })
+    // Moments
     .get('/moments/file', async function(req, res) {
-        console.log("------ Start request : GET /moments/file");
-
         const moments = await client.get('/moments/file');
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(moments));
-        console.log(JSON.stringify(moments));
-        console.log("------ Finished : GET /moments/file");
     })
+    // Liste des scénarios
     .get('/scenarios/file', async function(req, res) {
-        console.log("------ Start request : GET /scenarios/file");
-
         const scenarios = await client.get('/scenarios/file');
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(scenarios));
-        console.log(JSON.stringify(scenarios));
-        console.log("------ Finished : GET /scenarios/file");
     })
+    // Protocoles disponibles
     .get('/protocols', async function(req, res) {
-        console.log("------ Start request : GET /protocols");
-
         const protocols = await client.get('/protocols');
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(protocols));
-        console.log(JSON.stringify(protocols));
-        console.log("------ Finished : GET /protocols");
     })
+    // Etat d'un device
     .get('/device/:devicenum/endpoints/:endpointnum', async function(req, res) {
-        console.log("------ Start request : GET /devices/" + req.params.devicenum + "/endpoints/" + req.params.endpointnum );
-
         const info = await client.get('/devices/' + req.params.devicenum + '/endpoints/' + req.params.endpointnum + '/data');
         res.setHeader('Content-Type', 'application/json');
         res.setHeader('X-Request-DeviceId', req.params.devicenum);
         res.setHeader('X-Request-EndpointId', req.params.endpointnum);
         res.end(JSON.stringify(info));
-        console.log(JSON.stringify(info));
-        console.log("------ Finished : GET /devices");
     })
+    // Mise à jour d'un état d'un device
     .put('/device/:devicenum/endpoints/:endpointnum', async function(req, res) {
-        console.log("------ Start request : PUT /devices/" + req.params.devicenum + "/endpoints/" + req.params.endpointnum );
-
         const command = await client.put('/devices/' + req.params.devicenum + '/endpoints/' + req.params.endpointnum + '/data', [req.body]);
         res.setHeader('Content-Type', 'application/json');
         res.setHeader('X-Request-DeviceId', req.params.devicenum);
         res.setHeader('X-Request-EndpointId', req.params.endpointnum);
         res.end(JSON.stringify(command));
-        console.log(JSON.stringify(command));
-        console.log("------ Finished : PUT /devices")
     })
+    // Refresh des valeurs du jumeau numérique par rapport aux équipements physiques
     .post('/refresh/all', async function(req, res) {
-        console.log("------ Start request : POST /refresh/all")
-
         const refresh = await client.post('/refresh/all');
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(refresh));
-        console.log(JSON.stringify(refresh));
-        console.log("------ Finished : POST /refresh/all")
     })	
+    // Erreur
     .use(function(req, res, next){
-        console.log("------ Start request : GET " + req.url)
-
         res.setHeader('Content-Type', 'text/plain');
         res.status(404).send('Page introuvable !');
-
-        console.log("------ Finished : GET /" + req.url)
     });
 
     webServer = app.listen(port, function () {
