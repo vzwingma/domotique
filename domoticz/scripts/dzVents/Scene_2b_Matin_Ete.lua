fb95e4b92efb6f3306f27fa8ae6539a716dab0cc
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2021-10-19 15:06:37 +0200

    correction phase Matin Ete seulement en mode été

--- domoticz/scripts/dzVents/Scene_2b_Matin_Ete.lua
+++ domoticz/scripts/dzVents/Scene_2b_Matin_Ete.lua
@@ -10,12 +10,12 @@ return
     },
     execute = function(domoticz, scene)
         
-        -- Suivi de la phase du jour
-        domoticz.globalData.scenePhase = scene.name
-        
         -- Activation du groupe (le niveau est suivant le mode Domicile) - spécifique pour le mode Ete : on rebaisse le niveau
         local modeDomicile = domoticz.helpers.getModeDomicile(domoticz)
         if(modeDomicile == '_ete') then
+            -- Suivi de la phase du jour
+            domoticz.globalData.scenePhase = scene.name
+
             domoticz.log("Activation matin été pour le mode [" .. modeDomicile .."]", domoticz.LOG_INFO)
             domoticz.devices(domoticz.helpers.GROUPE_TOUS_VOLETS).setLevel(domoticz.variables(domoticz.helpers.VAR_PRCENT_VOLET_MATIN .. modeDomicile).value)
         end
