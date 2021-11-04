94de6d5b61b4e7974550435dda050c9f299c6f48
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2021-11-04 08:47:51 +0100

    Supression log & log debug. Lampe éteinte au matin

--- domoticz/scripts/dzVents/Supervision_pluie.lua
+++ domoticz/scripts/dzVents/Supervision_pluie.lua
@@ -3,7 +3,8 @@ return {
         timer = { 'every 30 minutes' }
     },
     data = {
-        previousRainRate = { initial = 0 }
+        previousRainRate = { initial = 0 },
+        previousRainState = { initial = 'Off' }
     },
     logging = {
         level = domoticz.LOG_INFO,
@@ -12,18 +13,22 @@ return {
     execute = function(domoticz, item)
         
         -- # Supervision du niveau de la pluie
-        function supervisionPluie(devicePluie)
-            domoticz.log(devicePluie.name .. ' ' .. devicePluie.rainRate .. ' mm.  [ ' .. devicePluie.rain .. ' mm/j ]')
+        function supervisionPluie(devicePluie, deviceIlPleut, domoticz)
+            domoticz.log(devicePluie.name .. ' ' .. devicePluie.rainRate .. ' mm.  [ ' .. devicePluie.rain .. ' mm/j ] : ' .. deviceIlPleut.state)
             -- Alerte si > 0.5
-            if(devicePluie.rainRate ~= nil and devicePluie.rainRate > 0.5 and devicePluie.rainRate ~= domoticz.data.previousRainRate) then
-                domoticz.log('Il pleut ' .. devicePluie.rainRate .. 'mm', domoticz.LOG_ERROR)
+            if(devicePluie.rainRate ~= nil and devicePluie.rainRate > 0.2 and devicePluie.rainRate ~= domoticz.data.previousRainRate) then
+                domoticz.log('Il pleut ' .. devicePluie.rainRate .. 'mm', domoticz.LOG_INFO)
                 domoticz.data.previousRainRate = devicePluie.rainRate
                 domoticz.helpers.notify('Alerte pluie : ' .. devicePluie.rainRate .. 'mm', domoticz)
             end
-
+            if(deviceIlPleut.state == 'On' and deviceIlPleut.state ~= domoticz.data.previousRainState) then
+                domoticz.log('Il pleut !', domoticz.LOG_INFO)
+                domoticz.helpers.notify('Alerte pluie', domoticz)
+                domoticz.data.previousRainState = deviceIlPleut.state
+            end
         end
 
         -- Supervision de la pluie        
-        supervisionPluie(domoticz.devices('Precipitation'))
+        supervisionPluie(domoticz.devices('Precipitation'), domoticz.devices('Il pleut ?'), domoticz)
     end
 }
