28e19c3bd515dd77d5038fc3edc0827970b1986a
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2021-11-08 22:26:28 +0100

    #57 : alignement des valeurs Phoscon sur un capteur virtuel

---
+++ domoticz/scripts/dzVents/Devices_TempHumidity.lua
@@ -0,0 +1,26 @@
+return {
+    on = {
+        devices = { 'Température - Chambre Bébé', 'Température - Chambre Nous', 'Température - Salle de Bain' }
+    },
+    data = {
+        previousPresenceTels = { initial = true },
+    },
+    logging = {
+        level = domoticz.LOG_INFO,
+        marker = "[TempHumidity] "
+    },
+    execute = function(domoticz, item)
+        
+        -- Notification par SMS lors du changement de nombre de connexions
+        function updateTempHumidity(itemName, domoticz)
+            local piece = string.sub(itemName, 16)
+            domoticz.log("Mise à jour des valeurs de Température et d'Humidité de ".. piece, domoticz.LOG_DEBUG)
+            local temp = domoticz.devices("Température - " .. piece).temperature
+            local humidity = domoticz.devices("Humidité - " .. piece).humidity
+            domoticz.log ("température=" .. temp .. "°C / humidité=" .. humidity .. "%", domoticz.LOG_DEBUG)
+            domoticz.devices("TempératureHumidité - " .. piece).updateTempHum(temp, humidity)
+        end
+
+        updateTempHumidity(item.name, domoticz)
+    end
+}
