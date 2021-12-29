b328beee98a9e210dae6d44f1b03b4908ae2d52f
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2021-12-29 21:31:21 +0100

    #70 mode normal

--- domoticz/scripts/dzVents/Device_Mode_Domicile.lua
+++ domoticz/scripts/dzVents/Device_Mode_Domicile.lua
@@ -11,15 +11,26 @@ return {
     },
     execute = function(domoticz, device)
 
-        -- Activation si passage en mode "Normal"
+        -- Replay de tous les scénarios jusqu'à la phase en cours, mais en mode "Normal"
+        function replaySceneInNormal(domoticz)
+            domoticz.log("Réactivation du scénario [" .. domoticz.globalData.scenePhase .. "]", domoticz.LOG_DEBUG) 
+            domoticz.scenes(domoticz.globalData.scenePhase).switchOn()
+            -- Thermostat
+            local tempMatin = domoticz.variables(domoticz.helpers.VAR_TEMPERATURE_MATIN).value
+            domoticz.log("Activation pour la journée Temp=[" .. tempMatin .. "°]")
+            domoticz.devices(domoticz.helpers.DEVICE_TYDOM_THERMOSTAT).updateSetPoint(tempMatin)
+        end
+
+
+
         local modeDomicile = device.levelName
         
         -- Notification lors du changement de mode, si changement
         if(modeDomicile ~= domoticz.data.previousMode) then
             domoticz.helpers.notify('Changement Mode : ' .. device.levelName, domoticz.helpers.uuid(), domoticz)
-            
+            -- Activation si passage en mode "Normal" , dans ce cas, on rejoue le scénario de la journée
             if(modeDomicile == 'Normal') then
-               domoticz.log("Réactivation du scénario : " .. domoticz.globalData.scenePhase) 
+               replaySceneInNormal(domoticz)
             end
             
         end
