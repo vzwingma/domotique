fa44882eeab39d7e93b2f664dcbd46d4cb1909f1
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2022-10-15 20:59:29 +0200

    Configuration de la connexion à la freebox

---
+++ domoticz/scripts/dzVents/Freebox_login.lua
@@ -0,0 +1,116 @@
+-- ## Scripts de login à la freebox pour mettre à jour le comportement Domoticz
+-- ## Paramètres nécessaires : 
+--  Variables Utilisateurs : 
+--      freebox_host : host de la freebox : mafreebox.freebox.fr/api/v9
+--      freebox_apptoken : app Token
+return {
+    on = {
+        timer = { 'every minute' },
+        httpResponses = { 'freebox_login', 'freebox_session' },
+        shellCommandResponses = { 'freebox_pwd' }
+    },
+    data = {
+        uuid = { initial = "" }
+    },
+    logging = {
+        level = domoticz.LOG_INFO,
+        marker = "[Freebox Delta] "
+    },
+    execute = function(domoticz, item)
+        
+    -- #### Fonctions de communication avec la Freebox
+
+        -- Authentification/ Login sur Freebox pour obtenir le challenge
+        function freeboxLogin(domoticz)
+    
+            domoticz.log("[" .. domoticz.data.uuid .. "] Authentification à la Freebox Delta", domoticz.LOG_DEBUG) 
+
+            local host_freebox = domoticz.variables(domoticz.helpers.VAR_FREEBOX_HOST).value
+            domoticz.openURL({
+                url = 'http://'..host_freebox..'' .. "/login",
+                method = 'GET',
+                headers = { ['Content-Type'] = 'application/json',  ['X-CorrId'] = corrId },
+                callback = 'freebox_login'
+            })
+        end
+    
+        -- Calcul du challenge pour avoir le password
+        function freeboxGetPassword(challenge, domoticz)
+    
+            domoticz.log("[" .. domoticz.data.uuid .. "] Calcul du mot de passe", domoticz.LOG_DEBUG) 
+            local apptoken_freebox = domoticz.variables(domoticz.helpers.VAR_FREEBOX_APP_TOKEN).value
+            local fullcmd = "echo -n '" .. challenge .. "' | openssl dgst -sha1 -hmac '" .. apptoken_freebox .. "' | cut -c10-200"
+            -- Appel de la session
+            domoticz.executeShellCommand({ 
+        	            command = fullcmd, 
+        	            callback = 'freebox_pwd' })
+            
+        end
+
+        -- Ouverture de session
+        freeboxOpenSession = function(passHmacSha1, domoticz)
+    
+            domoticz.log("[" .. domoticz.data.uuid .. "] Ouverture de session à la Freebox Delta", domoticz.LOG_DEBUG) 
+    
+            local host_freebox = domoticz.variables(domoticz.helpers.VAR_FREEBOX_HOST).value
+
+            -- Appel de la session
+            domoticz.openURL({
+                url = 'http://'..host_freebox..'' .. "/login/session",
+                method = 'POST',
+                headers = { ['Content-Type'] = 'application/json',  ['X-CorrId'] = corrId },
+                postData = {
+                            app_id = 'fr.freebox.domoticz',
+                            password = passHmacSha1
+                            },
+                callback = 'freebox_session'
+            })            
+            
+        end
+
+        -- Session ouverte sur la Freebox
+        function freeboxAuthenticated(session_token, domoticz)
+            domoticz.emitEvent('Freebox session', { data = session_token, uuid = domoticz.data.uuid })
+        end
+
+
+        
+    -- ## Déclenchement de la fonction /
+    if(item.isTimer) then
+        domoticz.data.uuid = domoticz.helpers.uuid()
+        -- d'abord l'Authentification
+        domoticz.log("[" .. domoticz.data.uuid .. "] Init de la connexion à la Freebox", domoticz.LOG_DEBUG)
+        freeboxLogin(domoticz)
+            
+    -- ## Call back après login
+    elseif(item.isHTTPResponse and item.callback == 'freebox_login') then 
+
+        domoticz.log("[" .. domoticz.data.uuid .. "] Login callback : " .. item.statusCode .. " - logged_in:" .. tostring(item.json.result.logged_in) , domoticz.LOG_DEBUG)
+        if(item.statusCode == 200) then
+            freeboxGetPassword(item.json.result.challenge, domoticz)
+        else 
+            domoticz.log("[" .. domoticz.data.uuid .. "] Erreur de connexion à la Freebox " .. item.statusCode .. " - " .. item.data , domoticz.LOG_ERROR)
+        end
+    -- ## Callback après calcul du HMAc SHA1
+    elseif(item.isShellCommandResponse and item.callback == 'freebox_pwd') then 
+
+        domoticz.log("[" .. domoticz.data.uuid .. "] Pwd callback : " .. item.statusCode .. " - HMAC Sha1:" .. item.data , domoticz.LOG_DEBUG)
+        if(item.statusCode == 0) then
+            freeboxOpenSession(item.data, domoticz)
+        else 
+            domoticz.log("[" .. domoticz.data.uuid .. "] Erreur de connexion à la Freebox " .. item.statusCode .. " - " .. item.data , domoticz.LOG_ERROR)
+        end
+
+    -- ## Call back après session
+    elseif(item.isHTTPResponse and  item.callback == 'freebox_session') then 
+            
+        if(item.statusCode == 200) then
+            domoticz.log("[" .. domoticz.data.uuid .. "] Session callback : " .. item.statusCode .. " - Session Token :" .. item.json.result.session_token , domoticz.LOG_DEBUG)
+            freeboxAuthenticated(item.json.result.session_token, domoticz)
+            
+        else 
+            domoticz.log("[" .. domoticz.data.uuid .. "] Erreur de connexion à la Freebox " .. item.statusCode .. " - " .. item.json.msg , domoticz.LOG_ERROR)
+        end
+    end
+end
+}
