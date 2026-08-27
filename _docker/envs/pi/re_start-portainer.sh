9ca82dbe08ade3dc1eb392b8ca401850699db723
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2026-08-27 15:56:47 +0200

    portainer

---
+++ _docker/envs/pi/re_start-portainer.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+HOME_PATH=/home/pi/appli
+DOCKER_PATH=$HOME_PATH/_docker
+
+echo ""
+echo "## Mise à jour des images docker ##"
+docker-compose -f $DOCKER_PATH/portainer-compose.yml pull
+echo ""
+echo "## (Re)création des conteneurs ## "
+
+docker-compose -f $DOCKER_PATH/portainer-compose.yml down --remove-orphans
+docker-compose -f $DOCKER_PATH/portainer-compose.yml up -d
